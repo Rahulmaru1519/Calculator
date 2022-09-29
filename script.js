d9b040be8ea89897ec7ca2ea715134b4dac62707
Rahulmaru1519 <83403559+Rahulmaru1519@users.noreply.github.com>
2022-09-29 14:14:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+//This function clears all the values
+function clearScreen(){
+    document.getElementById("result").value= "";
+}
+
+//This function displays values
+function display(value){
+    document.getElementById("result").value;
+}
+
+//This function evaluates the expression and return the results
+function calculator(){
+    var p = document.getElementById("result").value;
+    var q = eval(p);
+    document.getElementById("result").value = q;
+}
